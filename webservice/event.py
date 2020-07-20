6ecac58a383a503398c88a20c9abdbba47fcca20
randyTli <litianrun@baidu.com>
2020-07-20 23:35:02 +0800

    dummy commit

--- webservice/event.py
+++ webservice/event.py
@@ -1,5 +1,5 @@
 from gidgethub import routing
-from utils.check import checkPRNotCI, checkPRTemplate, checkComments, checkCIState
+from utils.check import checkPRCI, checkPRTemplate, checkComments
 from utils.readConfig import ReadConfig
 from utils.analyze_buildLog import ifDocumentFix, generateCiIndex, ifAlreadyExist
 from utils.db import Database
@@ -59,26 +59,28 @@ async def pull_request_event_ci(event, gh, repo, *args, **kwargs):
     commit_url = event.data["pull_request"]["commits_url"]
     sha = event.data["pull_request"]["head"]["sha"]
     base_branch = event.data["pull_request"]['base']['label']
-    if repo not in [
-            'PaddlePaddle/Paddle', 'PaddlePaddle/benchmark',
-            'lelelelelez/leetcode'
-    ]:
-        repo = 'Others'
     if base_branch.startswith(
             'PaddlePaddle:release') and repo == 'PaddlePaddle/Paddle':
         message = localConfig.cf.get(repo, 'PULL_REQUEST_OPENED')
-        logger.info("%s_%s Trigger CI Successful." % (pr_num, sha))
+        logger.info("%s Trigger CI Successful." % pr_num)
         if event.data['action'] == "opened":
             await gh.post(url, data={"body": message})
     else:
-        if checkPRNotCI(commit_url, sha) == False:
-            message = localConfig.cf.get(repo, 'PULL_REQUEST_OPENED')
-            logger.info("%s_%s Trigger CI Successful." % (pr_num, sha))
-        else:
-            message = "Hi, It's a test PR, it will not trigger CI. If you want to trigger CI, please remove `notest` in your commit message."
-            logger.info("%s_%s is a test PR." % (pr_num, sha))
-        if event.data['action'] == "opened":
+        if repo not in [
+                'PaddlePaddle/Paddle', 'PaddlePaddle/benchmark',
+                'lelelelelez/leetcode'
+        ]:
+            repo = 'Others'
+        CHECK_CI = localConfig.cf.get(repo, 'CHECK_CI')
+        if checkPRCI(commit_url, sha, CHECK_CI) == False:
+            message = localConfig.cf.get(repo, 'PULL_REQUEST_OPENED_NOT_CI')
+            logger.error("%s Not Trigger CI." % pr_num)
             await gh.post(url, data={"body": message})
+        else:
+            message = localConfig.cf.get(repo, 'PULL_REQUEST_OPENED')
+            logger.info("%s Trigger CI Successful." % pr_num)
+            if event.data['action'] == "opened":
+                await gh.post(url, data={"body": message})
 
 
 @router.register("pull_request", action="synchronize")
@@ -115,8 +117,8 @@ async def pull_request_event_template(event, gh, repo, *args, **kwargs):
                 message = localConfig.cf.get(repo, 'PR_CORRECT_DESCRIPTION')
                 logger.info("%s Correct PR Description and Meet Template" %
                             pr_num)
-                update_url = comment_list[i]['url']
-                await gh.patch(update_url, data={"body": message})
+                target_update_url = comment_list[i]['url']
+                await gh.patch(target_update_url, data={"body": message})
 
 
 @router.register("check_run", action="created")
@@ -199,57 +201,15 @@ async def check_ci_status(event, gh, repo, *args, **kwargs):
         context = event.data['context']
         status_dict['commitId'] = commitId
         status_dict['ciName'] = context
-        shortID = commitId[0:7]
-        comment_url = event.data["commit"]["comments_url"]
-        commit_url = event.data["commit"]["url"]
-        combined_statuses_url = commit_url + "/status"
-        combined_ci_status = checkCIState(combined_statuses_url)
         if state == 'success':
             commit_message = event.data['commit']['commit']['message']
             document_fix = ifDocumentFix(commit_message)
             if document_fix == False:
                 target_url = event.data['target_url']
                 generateCiIndex(repo, commitId, target_url)
-            if combined_ci_status == True:
-                message = localConfig.cf.get(repo, 'STATUS_CI_SUCCESS')
-                await gh.post(comment_url, data={"body": message})
         else:
-            print("commitID: %s" % shortID)
+            print("commitID: %s" % commitId)
             print("state : %s" % state)
-            comment_list = checkComments(comment_url)
-            failed_ci_link = event.data['target_url']
-            hyperlink_format = '<a href="{link}">{text}</a>'
-            xly_template = "🕵️Commit ID: <b>%s</b> contains failed CI.\r\n<b>🔍Failed: </b>"
-            tc_template = "🕵️Commit ID: <b>%s</b> contains failed CI.\r\n<b>🔍Failed: %s</b>"
-            if state in ['failure', 'error']:
-                if comment_list == '[]':
-                    if failed_ci_link.startswith('https://xly.bce.baidu.com'):
-                        failed_ci_hyperlink = hyperlink_format.format(
-                            link=failed_ci_link, text=context)
-                        error_message = xly_template % str(
-                            shortID) + failed_ci_hyperlink
-                        await gh.post(
-                            comment_url, data={"body": error_message})
-                    else:
-                        error_message = tc_template % (str(shortID), context)
-                        await gh.post(
-                            comment_url, data={"body": error_message})
-                else:
-                    latest_comment = comment_list[len(comment_list)]
-                    latest_body = latest_comment['body']
-                    update_url = latest_comment['url']
-                    if failed_ci_link.startswith('https://xly.bce.baidu.com'):
-                        failed_ci_hyperlink = hyperlink_format.format(
-                            link=failed_ci_link, text=context)
-                        update_message = latest_body + "\r\n" + xly_template % str(
-                            shortID) + failed_ci_hyperlink
-                        await gh.patch(
-                            update_url, data={"body": update_message})
-                    else:
-                        update_message = latest_body + "\r\n" + tc_template % (
-                            str(shortID), context)
-                        await gh.patch(
-                            update_url, data={"body": update_message})
         if state in ['success', 'failure']:
             ifInsert = True
             status_dict['status'] = state
