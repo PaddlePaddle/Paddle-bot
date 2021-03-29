226075e552535a33737aa7c1993ee981fe38341e
Paddle CI_MAC <paddle_ci@example.com>
2021-03-29 18:23:17 +0800

    issue迁移至gitee

---
+++ giteeservice/Issue_main.py
@@ -0,0 +1,19 @@
+import GithubLabelToGitee
+import GithubToGitee
+
+
+github_header = {'User-Agent': 'Mozilla/5.0',
+            'Authorization': '',
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+            }
+create_token = ""
+close_token = ""
+repo_list = ['']
+
+
+for repo in repo_list:
+    label_app = GithubLabelToGitee.GithubLabelToGitee(repo, github_header, close_token)
+    label_app.CreateGiteeLabel()
+    issue_app = GithubToGitee.GithubIssueToGitee(repo, github_header, create_token)
+    issue_app.CreateIssueToGitee(close_token)
