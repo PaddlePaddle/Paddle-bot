f8fe3470b2dae7d483ec7a2bf7bde7788384d865
iducn <iducn3@163.com>
2021-07-15 14:45:50 +0800

    新增监控并获取merged PR的fluidInferenceSize_so数据

--- webservice/monitor/fluidInferenceSize.py
+++ webservice/monitor/fluidInferenceSize.py
@@ -1,27 +1,82 @@
 import requests
+import datetime
 import argparse
 import sys
 import re
 import os
 
-sys.path.append("xxxxx")
+sys.path.append("xx")
 from utils.db import Database
 from utils import bosclient
 
 
-def GetCommits(headers, PR):
-    commits_list = []
-    url = 'https://api.github.com/repos/PaddlePaddle/Paddle/pulls/%s/commits' % PR
-    print(url)
+def toBJtime(Time):
+    """
+    utc时间转换北京时间
+    """
+    Time = Time.replace('T', ' ').replace('Z', '')
+    Time = datetime.datetime.strptime(Time, '%Y-%m-%d %H:%M:%S')
+    Time = Time + datetime.timedelta(hours=8)
+    Time = datetime.datetime.strftime(Time, '%Y-%m-%d %H:%M')
+    return Time
+
+
+def PageNum(headers):
+    """
+    获取返回信息中的总页数
+    """
+    url = "https://api.github.com/repos/PaddlePaddle/Paddle/pulls?state=closed&per_page=100&sort=merged_at"
+    msg = requests.get(url, headers=headers)
+    # 获取头信息中的Link内容
+    header_info = msg.headers["Link"]
+    # 消除<>和空格
+    header_replace = re.sub('<|>| ', '', header_info)
+    # 以,和;分割成一个列表
+    header_split = re.split(',|;', header_replace)
+    # 获取列表中rel="last"的索引
+    last_index = header_split.index('rel=\"last\"')
+    # 获取last的url链接
+    num = header_split[last_index - 1]
+    # 获取last的url中的页码
+    page_num = re.search('&page=(\d+)', num)
+    page_num = str(page_num.group(1))
+    url = url + "&page=" + page_num
+    return url
+
+
+def Record_PR(PR):
+    """
+    记录已经获取过数据的PR
+    """
+    with open(dir_path + 'xx', "a+", encoding='utf-8') as f:
+        f.write("%s," % str(PR))
+
+
+def GetCommits(headers, url, dir_path):
+    """
+    获取PR中的commit ID
+    """
     response = requests.get(url, headers=headers).json()
-    for commit in response:
-        sha = commit['sha']
-        if sha not in commits_list:
-            commits_list.append(sha)
+    commits_list = []
+    for info in response:
+        PR = str(info['number'])
+        f = open(dir_path + 'xx', "r", encoding='utf-8')
+        if PR not in f.read():
+            Record_PR(PR)
+            url = 'https://api.github.com/repos/PaddlePaddle/Paddle/pulls/%s/commits' % PR
+            commit_response = requests.get(url, headers=headers).json()
+            for commit in commit_response:
+                sha = commit['sha']
+                if sha not in commits_list:
+                    commits_list.append(sha)
+        f.close()
     return commits_list
 
 
 def Info(sha):
+    """
+    从数据库中获取该信息
+    """
     sql = "select fluidInferenceSize_so from paddle_ci_index where ciName='PR-CI-Inference' and commitId='%s' order by time desc limit 100;" % sha
     db = Database()
     data = list(db.query(sql))
@@ -31,26 +86,26 @@ def Info(sha):
     return False
 
 
-def Save(data, dir_name):
-    if not os.path.exists(dir_name):
-        os.makedirs(dir_name)
+def Save(data, dir_path):
+    """
+    保存fluidInference_so_size数据
+    """
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
     with open(
-            dir_name + "fluidInference_so_size", "w+", encoding='utf-8') as f:
+            dir_path + "fluidInference_so_size", "w+", encoding='utf-8') as f:
         f.write(str(data))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--PR', help='the day of dates.', default=None)
-    args = parser.parse_args()
-    headers = {'authorization': "xxxxx"}
-
-    if args.PR:
-        commits_list = GetCommits(headers, args.PR)
-        for sha in commits_list:
-            data = Info(sha)
-            if data:
-                Save(data, "path_name")
-    bosclient.uploading("fluidInference_so_size")
+    headers = {'authorization': "token xx"}
+    dir_path = 'xx'
+    url = PageNum(headers)
+    commits_list = GetCommits(headers, url, dir_path)
+    for sha in commits_list:
+        data = Info(sha)
+        if data:
+            print(sha)
+            print(data)
+            Save(data, dir_path)
+            bosclient.uploading("fluidInference_so_size")
